a66c83450d48a9197d3e7e723c1d2b6123f50e09
velizartodorov <willking66@gmail.com>
2026-07-02 16:44:01 +0200

    :art: standardize access-specifier formatting across headers
    HillCipher.h/StreamCipher.h used tab-indented `public:` (one tab
    under the class, members at two tabs), predating this session's
    changes; Utils.h/Matrix.h use flush-left public:/private: with
    4-space-indented members. Standardize all four headers on the
    flush-left style.
    
    No behavior change - pure whitespace. Verified with a rebuild and all
    14 tests passing.

--- HillCipher/HillCipher.h
+++ HillCipher/HillCipher.h
@@ -4,7 +4,7 @@
 using namespace std;
 
 class HillCipher {
-	public:
-		// throws invalid_argument if matrixKey is empty or not square
-		void encode(string& plainTxt, const Matrix& matrixKey);
+public:
+    // throws invalid_argument if matrixKey is empty or not square
+    void encode(string& plainTxt, const Matrix& matrixKey);
 };
--- StreamCipher/StreamCipher.h
+++ StreamCipher/StreamCipher.h
@@ -5,7 +5,7 @@
 using namespace std;
 
 class StreamCipher {
-	public:
-		void encode(const string& plainText, string& cipherText, const string& charKey);
+public:
+    void encode(const string& plainText, string& cipherText, const string& charKey);
 };
 #endif
